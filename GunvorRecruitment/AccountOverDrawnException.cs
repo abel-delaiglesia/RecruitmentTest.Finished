f768c07e7649e110f9e6e94d4005236de2abc51b
Abel de la Iglesia <joseabel.delaiglesia@gmail.com>
2020-03-18 21:39:39 +0100

    Agregar archivos de proyecto.

---
+++ GunvorRecruitment/AccountOverDrawnException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GunvorRecruitment
+{
+    public class AccountOverDrawnException : Exception
+    {
+    }
+}
---
+++ GunvorRecruitment/CurrentAccount.cs
@@ -0,0 +1,25 @@
+﻿namespace GunvorRecruitment
+{
+    public class CurrentAccount
+    {
+        public decimal Balance { get; private set; }
+
+        public int OverDraft { get; set; }
+
+        public string PersonName { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            var tempValue = Balance;
+            var newValue = tempValue + amount;
+            Balance = newValue;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            var tempValue = Balance;
+            var newValue = tempValue - amount;
+            Balance = newValue;
+        }
+    }
+}
